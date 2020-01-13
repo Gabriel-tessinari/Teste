bfc09c882eec3d75a2fa0aeb7c9fa63ca2f7316b
Gabriel Tessinari <gtcmiranda18@gmail.com>
2020-01-13 19:40:16 -0300

    Aula: Gráficos com java - Frame e Canvas

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,36 @@
 package main;
 
-public class Main {
+import java.awt.Canvas;
+import java.awt.Dimension;
 
+import javax.swing.JFrame;
+
+public class Main extends Canvas implements Runnable {
+
+	public static JFrame frame;
+	private final int WIDTH = 160;
+	private final int HEIGTH = 120;
+	private final int SCALE = 3;
+	
+	public Main() {
+		
+		this.setPreferredSize(new Dimension(WIDTH*SCALE, HEIGTH*SCALE));
+		
+		frame = new JFrame();
+		frame.add(this);
+		frame.setResizable(false);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		
+		Main screen = new Main();
+	}
+	
+	public void run() {
+		
+	}
 }
